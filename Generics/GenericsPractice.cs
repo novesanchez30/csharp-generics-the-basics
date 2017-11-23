3b361c323d6d56f60c05b15040dddb1909cab437
sancheznove <nove.sanchez@gmail.com>
2017-11-23 12:55:06 +0800

    add project files

---
+++ Generics/GenericsPractice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGenericsImplementation
+{
+	/**
+	* @author: Nove Sanchez Nov. 23, 2017
+	* @description:
+	* this is just a basic implementation of Generics for beginners
+	* instead of hardcoding the specific type
+	* with generics we can implement code reuse with type safety
+	* this is to avoid type casting and unboxing/boxing
+	* 
+	* T is an arbitrary that most dev follow
+	* T is a placeholder whenever we provide a type
+	* 
+	* @reference: wwww.pluralsight.com
+	* 
+	* feel free to download and modify the code for testing purposes. :)
+	*/
+	class GenericClass<T>//T is a type parameter
+	{
+		private T _localGenericVariable;//local variable with T type
+		
+		public GenericClass(T value)//class constructor with T type
+		{
+			Console.WriteLine("constructor value: {0}", value);
+			_localGenericVariable = value;
+		}
+		
+		public T GenericMethod(T genericParameter)//method with T return type and T as type parameter
+		{
+			Console.WriteLine("return type of genericParameter {0}, value: {1}", typeof(T).ToString(), genericParameter);
+			Console.WriteLine("return type of _localVariable {0}, value: {1}", typeof(T).ToString(), _localGenericVariable);
+
+			return _localGenericVariable;
+		}
+	}
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleGenericsImplementation;
+
+namespace Generics
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GenericClass<int> gc_int = new GenericClass<int>(10);//<int> is the type argument
+			gc_int.GenericMethod(20);
+
+			GenericClass<double> gc_db = new GenericClass<double>(99.11);//<double> is the type argument
+			gc_db.GenericMethod(99.10);
+
+			GenericClass<string> gc_str = new GenericClass<string>("I am Prince");//<string> is the type argument
+			gc_str.GenericMethod("I am Fritz");
+			
+			object obj = 1000;
+			GenericClass<object> gc_ob = new GenericClass<object>(obj);//<object> is the type argument
+			gc_ob.GenericMethod(obj);
+			//by using type argument object, this will allow you to put a value any type like string, double, etc
+
+			Console.ReadKey();
+		}
+	}
+}
